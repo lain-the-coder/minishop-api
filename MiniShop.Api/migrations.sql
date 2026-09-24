f4d13ccbc311646955acbfb62976296a0b4e83a9
Ehab Ahmed <ahmed.ehabj@gmail.com>
2026-09-24 19:35:42 +0400

    feat(migration-script): generated migration script output for production scnerios via ef core cli

---
+++ MiniShop.Api/migrations.sql
@@ -0,0 +1,130 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260924135401_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Products] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(200) NOT NULL,
+        [Price] decimal(18,2) NOT NULL,
+        [Stock] int NOT NULL,
+        CONSTRAINT [PK_Products] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260924135401_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Users] (
+        [Id] int NOT NULL IDENTITY,
+        [Email] nvarchar(256) NOT NULL,
+        CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260924135401_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Orders] (
+        [Id] int NOT NULL IDENTITY,
+        [UserId] int NOT NULL,
+        [Status] nvarchar(20) NOT NULL,
+        [CreatedAt] datetime2 NOT NULL,
+        CONSTRAINT [PK_Orders] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Orders_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260924135401_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [OrderItems] (
+        [Id] int NOT NULL IDENTITY,
+        [OrderId] int NOT NULL,
+        [ProductId] int NOT NULL,
+        [Quantity] int NOT NULL,
+        [PriceAtPurchase] decimal(18,2) NOT NULL,
+        CONSTRAINT [PK_OrderItems] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_OrderItems_Orders_OrderId] FOREIGN KEY ([OrderId]) REFERENCES [Orders] ([Id]) ON DELETE NO ACTION,
+        CONSTRAINT [FK_OrderItems_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260924135401_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_OrderItems_OrderId] ON [OrderItems] ([OrderId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260924135401_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_OrderItems_ProductId] ON [OrderItems] ([ProductId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260924135401_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_Orders_UserId] ON [Orders] ([UserId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260924135401_InitialCreate'
+)
+BEGIN
+    CREATE UNIQUE INDEX [IX_Users_Email] ON [Users] ([Email]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260924135401_InitialCreate'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260924135401_InitialCreate', N'9.0.20');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260924144037_AddProductDescription'
+)
+BEGIN
+    ALTER TABLE [Products] ADD [Description] nvarchar(2000) NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260924144037_AddProductDescription'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260924144037_AddProductDescription', N'9.0.20');
+END;
+
+COMMIT;
+GO
+
